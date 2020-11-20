dd9e0140b199809e82ea499ea2996e524d0d900d
Sukhvsin2 <sukhvsingh2026@gmail.com>
2020-11-21 00:09:19 +0530

    final commit

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,29 +1,79 @@
-import React, { useState, useEffect } from 'react'
+import React, {
+    useState,
+    useEffect
+} from 'react'
 import queryString from "query-string"
 import io from "socket.io-client"
-import axios from "axios"
+import Infobar from "../Infobar/Infobar"
+import "./Chat.css"
+import Input from '../Input/Input'
+import Messages from '../Messages/Messages'
 
 let socket;
 
-function Chat({ location }) {
+function Chat({
+    location
+}) {
     const [name, setName] = useState('')
     const [room, setRoom] = useState('')
+    const [users, setUsers] = useState([])
+    const [message, setMessage] = useState([])
+    const [messages, setMessages] = useState('')
+
+    const ENDPOINT = 'localhost:5000'
 
     useEffect(() => {
-        axios.get('localhost:2727', {data: 'check'}).then(res => {
-            console.log('check', res);
-        })
-        const { name, room } = queryString.parse(location.search)
-        
-        socket = io('localhost:5000')
+        const {
+            name,
+            room
+        } = queryString.parse(location.search)
+
+        socket = io(ENDPOINT)
 
         setName(name)
         setRoom(room)
-        console.log(socket);
-    },[])
+        
+        socket.emit('join', { name, room }, (error) => {
+            if(error) console.log('socket error manuall - ', error);
+        });
+
+        return () => {
+            // for disconnecting
+            socket.emit('disconnect')
+            // for turning off the socket instance
+            socket.off()
+        }
+    }, [location.search])
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message])
+        })
+
+        socket.on('roomData', ({ users }) => {
+            setUsers(users)
+        })
+    }, [messages])
+
+    
+    function sendMessage(event) {
+        event.preventDefault()
+
+        
+        if (message) {
+            socket.emit('sendMessage', message, () => setMessage(''))
+        }
+    }
+
+    console.log(messages);
+
     return (
-        <div>
-            Chat
+        <div className="outerContainer">
+            <div className="container">
+                <Infobar RoomName={room} users={users} />
+                <Messages messages={messages} name={name} />
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+            </div>
         </div>
     )
 }
